dea17c0eb1e12c6bb965b0ad3e1cea91f991b199
ikachko <ilyakachko@gmail.com>
2019-03-20 19:46:37 +0200

    validation finished

--- solver.py
+++ solver.py
@@ -8,6 +8,55 @@ def print_usage():
     print("Usage:\nsolver.py file_name")
 
 
+def read_from_stdin():
+    data = sys.stdin.readlines()
+    return validate_data(data)
+
+def validate_data(data):
+    line_length = 0
+    num_of_lines = 0
+
+    matrix = []
+
+    for line in data:
+        if line[0] == '#':
+            continue
+        line = line.split('#')[0]
+        if num_of_lines == 0:
+            spl = line.split(' ')
+            if type(spl == str):
+                num_of_lines = int(line)
+            else:
+                print("Error: wrong matrix size parameter")
+                return -1
+            if num_of_lines <= 2:
+                print("Size must be > 2")
+                return -1
+            continue
+        str_nums = list(filter(None, line.split(' ')))
+        if type(str_nums) != list:
+            print("Error")
+            return -1
+
+        num_count = len(str_nums)
+        if line_length == 0:
+            line_length = num_count
+        elif line_length != num_count:
+            print("Error: number of digits must be the same")
+            return -1
+
+        numbers = [int(n) for n in str_nums]
+        matrix.append(numbers)
+    if not matrix:
+        return -1
+    if len(matrix) != len(matrix[0]):
+        print("Error: puzzle must be squared")
+        return -1
+    if len(matrix) != num_of_lines:
+        print("Error: invalid size")
+        return -1
+    return matrix
+
 def read_from_file(file_name: str):
     try:
         f = open(file_name, 'r')
@@ -15,70 +64,37 @@ def read_from_file(file_name: str):
         print("Could not read file: ", file_name)
         return -1
 
-    line_length = 0
-    num_of_lines = 0
-
-    matrix = []
-
-    while f.readable():
-        line = f.readline()[:-1].strip()
-        if not line:
-            break
-        if line[0] == '#':
-            continue
-        if num_of_lines == 0:
-            if type(line.split(' ') == str):
-                num_of_lines = int(line)
-            else:
-                print("Error")
-                return -1
-        for i in range(num_of_lines):
-            line = f.readline()[:-1].strip()
-            str_nums = list(filter(None, line.split(' ')))
-
-            if type(str_nums) != list:
-                print("Error")
-                return -1
-
-            num_count = len(str_nums)
-            if line_length == 0:
-                line_length = num_count
-            elif line_length != num_count:
-                print("Error: number of digits must be the same")
-                return -1
-
-            numbers = [int(n) for n in str_nums]
-            matrix.append(numbers)
-    if len(matrix) != len(matrix[0]):
-        print("Error: puzzle must be squared")
-        return -1
+    data = list(filter(None, f.read().split('\n')))
+    matrix = validate_data(data)
     return matrix
 
+
 def is_valid(puzzle):
-    puzzle_str = list(''.join(str(item) for innerlist in outerlist for item in innerlist)).sort()
-    puzzle_set_str = list(set(puzzle_str)).sort()
-    if puzzle_str != puzzle_set_str or '0' not in puzzle_str:
-        print(puzzle_str)
-        print(puzzle_set_str)
+    puzzle_line = []
+    for line in puzzle:
+        puzzle_line += line
+    puzzle_line.sort()
+    if puzzle_line[-1] != (len(puzzle) ** 2 - 1):
+        return False
+    puzzle_set = list(set(puzzle_line))
+    puzzle_set.sort()
+    if puzzle_line != puzzle_set or 0 not in puzzle_line:
         return False
     return True
 
 def get_inv_count(puzzle):
     inv_count = 0
-    for i in range(len(puzzle) - 1):
-        for j in range(i + 1, len(puzzle[i])):
-            if (puzzle[i] > puzzle[j]):
+    puzzle_line = []
+    for line in puzzle:
+        puzzle_line += line
+    for i in range(len(puzzle_line) - 1):
+        for j in range(i + 1, len(puzzle_line)):
+            if puzzle_line[i] > puzzle_line[j]:
                 inv_count += 1
     return inv_count
 
 def is_solvable(puzzle):
-    if not is_valid(puzzle):
-        return False
-
-    inv_count = get_inv_count(puzzle)
-    if inv_count % 2 == 0:
-        return True
-    return False
+    return is_valid(puzzle) and (get_inv_count(puzzle) % 2 == 0)
 
 def make_goal(s):
     ts = s*s
@@ -106,7 +122,6 @@ def make_goal(s):
     goal = [puzzle[i:i + s] for i in range(0, len(puzzle), s)]
     return goal
 
-
 def main():
     argv_len = len(sys.argv)
     if argv_len > 2 or argv_len <= 0:
@@ -115,11 +130,12 @@ def main():
     elif argv_len == 2:
         matrix = read_from_file(sys.argv[1])
     else:
-        matrix = -1
-    # else:
-    #     matrix = read_from_stdin()
-
+        matrix = read_from_stdin()
     if matrix == -1:
+        return
+
+    if not is_solvable(matrix):
+        print("Matrix is not solvable/is not valid")
         return
     print("\nMatrix to solve:")
     for line in matrix:
